d4f7f037b7b09ef37f00bb18f5f84e89767ae591
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2024-03-09 15:41:57 +0800

    Update Bonezegei_MAX7219.h

--- src/Bonezegei_MAX7219.h
+++ src/Bonezegei_MAX7219.h
@@ -3,8 +3,11 @@ class Bonezegei_MAX7219{
 public:
   Bonezegei_MAX7219();
   Bonezegei_MAX7219(uint8_t cs);
+  Bonezegei_MAX7219(uint8_t dat, uint8_t clk, uint8_t cs);
 
   char begin();
 
-  uint8_t _cs
+  uint8_t _cs;
+  uint8_t _dat; //using shift
+  uint8_t _clk; //using shift
 }
